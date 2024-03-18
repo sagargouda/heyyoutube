8bf9feaa09b4a0e2433c0f57ff7178a0e4c0bd62
sagar <sagarc2580@gmail.com>
2024-03-18 05:37:02 +0530

    feat: add video container and video card

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import Button from "./Button";
 function ButtonList(props) {
     return (
-        <div>
-
+        <div className="hidden  md:flex md:flex-wrap md:justify-center ">
+            <Button name="All"/>
+            <Button name="Kondzilla"/>
+            <Button name="World"/>
+            <Button name="Kanye West"/>
+            <Button name="NF"/>
+            <Button name="Make Money "/>
+            <Button name="Interviews"/>
+            <Button name="Sasha"/>
         </div>
     );
 }
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,35 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import {YOUTUBE_VIDEO_API} from "../utils/constants";
+import {useState} from "react";
+import VideoCard from "./VideoCard";
 function VideoContainer(props) {
-    return (
-        <div></div>
+
+    const [ videos , setVideos ]= useState([])
+
+    useEffect(() => {
+        getVideos()
+    }, []);
+
+
+    async function getVideos(){
+        const data = await fetch(YOUTUBE_VIDEO_API)
+        const json = await data.json()
+        console.log(json)
+        setVideos(json.items)
+    }
+
+
+
+    return (<div className=" mx-auto flex flex-wrap justify-center">
+            {
+                videos.map((video)=>{
+                    return  <VideoCard key={video.id} info={video}/>
+                })
+            }
+
+        </div>
     );
+
 }
 
 export default VideoContainer;
